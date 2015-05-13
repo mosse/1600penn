a8af5d537bea50139c360bc8b6df50dff718ad86
Mark Macdonald <mjemacdonald@gmail.com>
2015-05-13 12:37:19 -0700

    (setup) Implements basic Jade templating

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layout', { title: 'Who\'s been to 1600 Penn' });
+  res.render('index', { title: 'Who\'s been to 1600 Penn?' });
 });
 
 module.exports = router;
