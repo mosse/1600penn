eac54f380cce9ad2a5320dc896069372384543c5
Mark Macdonald <mjemacdonald@gmail.com>
2015-06-01 14:21:36 -0700

    (setup) Adds backbone folder structure

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Who\'s been to 1600 Penn?' });
-});
-
-module.exports = router;
